bcf9e4812bd595f4e5ccba4ff4760060aca954fe
qin2 <qin2@qq.com>
2017-04-25 10:06:22 +0800

    01

--- ColorMaker/main.cpp
+++ ColorMaker/main.cpp
@@ -1,12 +1,31 @@
 #include <QGuiApplication>
-#include <QQmlApplicationEngine>
+#include <QtQuick/QQuickView>
+#include <QtQml>
+#include "colormaker.h"
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
-    QQmlApplicationEngine engine;
-    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+    /*QQuickView viewer;
+
+    QObject::connect(viewer.engine(), SIGNAL(quit()), &app, SLOT(quit()));
+
+    viewer.setResizeMode(QQuickView::SizeRootObjectToView);
+
+    viewer.rootContext()->setContextProperty("colorMaker", new ColorMaker);
+
+    viewer.setSource(QUrl("qrc:/main.qml"));
+    viewer.show();*/
+
+    qmlRegisterType<ColorMaker>("an.qt.ColorMaker", 1, 0, "ColorMaker");
+    QQuickView viewer;
+
+    QObject::connect(viewer.engine(), SIGNAL(quit()), &app, SLOT(quit()));
+
+    viewer.setResizeMode(QQuickView::SizeRootObjectToView);
+    viewer.setSource(QUrl("qrc:/main.qml"));
+    viewer.show();
 
     return app.exec();
 }
