fbce3f95f6626d83d8776a18af407384493457a1
qin2 <qin2@qq.com>
2018-05-12 16:37:29 +0800

    update TestProtobuf

--- TestProtobuf/TestProtobuf.pro
+++ TestProtobuf/TestProtobuf.pro
@@ -14,4 +14,14 @@ DEFINES += QT_DEPRECATED_WARNINGS
 # You can also select to disable deprecated APIs only up to a certain version of Qt.
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
-SOURCES += main.cpp
+SOURCES += main.cpp \
+    people.pb.cc
+
+DISTFILES += \
+    people.proto
+
+LIBS += /usr/local/lib/libprotobuf.a
+INCLUDEPATH += /usr/local/include/
+
+HEADERS += \
+    people.pb.h
--- TestProtobuf/main.cpp
+++ TestProtobuf/main.cpp
@@ -1,8 +1,37 @@
-#include <QCoreApplication>
+#include <iostream>
+
+#include "people.pb.h"
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+    test::People pep;
+    pep.set_name("heheda");
+    pep.set_age(5);
+    pep.set_steps(1304);
 
-    return a.exec();
+    std::string buff;
+    pep.SerializeToString(&buff);
+
+    test::People pep2;
+    pep2.ParseFromString(buff);
+
+    std::cout << pep2.name() << std::endl;
+    std::cout << pep2.age() << std::endl;
+    std::cout << pep2.steps() << std::endl;
+
+    int data_len = pep.ByteSize();
+    unsigned char data[1024] = "\0";
+    pep.SerializeToArray(data, data_len);
+    for(auto i = 0; i < data_len; i ++)
+        printf("%02X ", data[i]);
+    printf("\n");
+
+    test::People pep3;
+    pep3.ParseFromArray(data, data_len);
+
+    std::cout << pep3.name() << std::endl;
+    std::cout << pep3.age() << std::endl;
+    std::cout << pep3.steps() << std::endl;
+
+    return 0;
 }
