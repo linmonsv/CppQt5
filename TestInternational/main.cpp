9d5568e5783251bf1c2af50b9f665e94629a3ceb
qin2 <qin2@qq.com>
2018-05-13 19:48:11 +0800

    update TestInternationl

--- TestInternational/main.cpp
+++ TestInternational/main.cpp
@@ -1,9 +1,18 @@
 #include "widget.h"
 #include <QApplication>
 
+#include <QTranslator>
+
+#include <QDebug>
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QTranslator trans;
+     if(trans.load(a.applicationDirPath() + "/../../../zh_CN.qm"))
+        a.installTranslator(&trans);
+
     Widget w;
     w.show();
 
